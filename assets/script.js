d827c7a0c1a2367020a30c6558bb137d5ec6c25b
Scott Everett <burningcupidxxx@gmail.com>
2022-01-16 01:16:10 -0800

    restrict past searches display to 8 items, add units to weather readings

--- assets/script.js
+++ assets/script.js
@@ -4,6 +4,8 @@ var foreCast2 = moment().add(2, 'days').calendar(`M${"/"}D${"/"}YYYY`);
 var foreCast3 = moment().add(3, 'days').calendar(`M${"/"}D${"/"}YYYY`);
 var foreCast4 = moment().add(4, 'days').calendar(`M${"/"}D${"/"}YYYY`);
 var foreCast5 = moment().add(5, 'days').calendar(`M${"/"}D${"/"}YYYY`);
+var searchList = $("#search-list")
+var searches = [];
 var searchForm = document.querySelector("#search-form");
 var searchInputEl = $("#search-bar");
 var cityNameEl = $("#city-name-display");
@@ -18,10 +20,89 @@ var apiKey = "672c0e16208bfbcdccb5e0e26b7d03b2";
 var tempSpan = $("#temp-span");
 var humiditySpan = $("#humidity-span");
 var windSpeedSpan = $("#wind-speed-span");
-var uvSpan = $("uv-span");
+var uvSpan = $("#uv-span");
 var forecastContainer = $("#five-day-container");
 
 var forecast = [foreCast1, foreCast2, foreCast3, foreCast4, foreCast5];
+
+
+//Calling function init();
+init();
+
+//Function init();
+function init(){
+    //Get stored cities from localStorage
+    //Parsing the JSON string to an object
+    var storedSearches = JSON.parse(localStorage.getItem("searches"));
+
+    // If cities were retrieved from localStorage, update the cities array to it
+    if (storedSearches !== null) {
+        searches = storedSearches;
+      }
+    // Render cities to the DOM
+    renderSearches();
+    // console.log(cities);
+}
+
+//Function StoreCities()
+function storeSearches(){
+   // Stringify and set "cities" key in localStorage to cities array
+  localStorage.setItem("searches", JSON.stringify(searches));
+  console.log(localStorage);
+}
+
+//Function renderCities()
+function renderSearches() {
+    // Clear cityList element
+    // cityList.text = "";
+    // cityList.HTML = "";
+    searchList.empty();
+    
+    // Render a new li for each city
+    for (var i = 0; i < 8; i++) {
+      var search = searches[i];
+      
+      var li = $("<li>").text(search);
+      li.attr("id","listC");
+      li.attr("data-search", search);
+      li.attr("class", "list-group-item");
+      console.log(li);
+      searchList.prepend(li);
+    }
+    //Get Response weather for the first city only
+    // if (!search){
+    //     return
+    // } 
+    // else{
+    //     getResponseWeather(search)
+    // };
+}   
+
+  //When form is submitted...
+  $(searchForm).on("submit", function(event){
+      event.preventDefault();
+
+    // This line will grab the city from the input box
+    var search = searchInputEl.val().trim();
+    
+    // Return from function early if submitted city is blank
+    if (search === "") {
+        return;
+    }
+    //Adding city-input to the city array
+    searches.push(search);
+    // Store updated cities in localStorage, re-render the list
+  storeSearches();
+  renderSearches();
+  });
+
+
+
+
+
+
+
+
 
 
 
@@ -29,7 +110,8 @@ var forecast = [foreCast1, foreCast2, foreCast3, foreCast4, foreCast5];
 
 var formSubmitHandler = function (event) {
     event.preventDefault();
-  
+    // forecastContainer.remove(forecastDisplay);
+
     var cityName = searchElVanilla.value.trim();
   
     if (cityName) {
@@ -72,9 +154,10 @@ function apiQuery() {
         console.log(oneCallData);
 
         // display data
-        tempSpan.text(oneCallData.current.temp);
-        humiditySpan.text(oneCallData.current.humidity);
-        windSpeedSpan.text(oneCallData.current.wind_speed);
+        tempSpan.text(oneCallData.current.temp + ` \xB0F`);
+        humiditySpan.text(oneCallData.current.humidity + "%");
+        windSpeedSpan.text(oneCallData.current.wind_speed + " mph");
+        uvSpan.text(oneCallData.current.uvi);
 
         //dynamically create forecast cards 
 
@@ -82,6 +165,9 @@ function apiQuery() {
         // forecastContainer.append(forecastCard); 
         // forecastCard.append(forecastDateEl);
         // forecastDateEl.text(foreCast1);
+
+        // empty five day forecast section of past content
+        $("#five-day-container").empty();
 
         for (i = 0; i < 5; i++) {
             var forecastCard = $("<div>");
@@ -96,7 +182,6 @@ function apiQuery() {
             forecastDateEl.text(forecast[i]);
             forecastTempEl.text("Temp: " + oneCallData.daily[i].temp.day);
             forecastHumidityEl.text("Humidity: " + oneCallData.daily[i].humidity)
-            console.log(i);
         }
         
 
